d73a15c2e396b52e54f6a0d2b21180770f378012
pchchv <ipchchv@gmail.com>
2023-12-25 18:42:26 +0300

    Implement an assert frame

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -1,5 +1,11 @@
 package stack
 
+import (
+	"fmt"
+	"regexp"
+	"testing"
+)
+
 type someT struct{}
 
 func (s someT) level2() *Trace {
@@ -24,4 +30,19 @@ func level1() *Trace {
 
 func level0() *Trace {
 	return level1()
+}
+
+func assertFrame(t *testing.T, frame *Frame, file string, line int, fun string) {
+	testName := fmt.Sprintf("[file: %s line: %d fun: %s]", file, line, fun)
+	if !regexp.MustCompile(file).MatchString(frame.File) {
+		t.Errorf("assertFrame: %s didn't match file in %v", testName, frame)
+	}
+
+	if frame.LineNumber != line {
+		t.Errorf("assertFrame: %s didn't match line in %v", testName, frame)
+	}
+
+	if frame.Name != fun {
+		t.Errorf("assertFrame: %s didn't match function name in %v", testName, frame)
+	}
 }
