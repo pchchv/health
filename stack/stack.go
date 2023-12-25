820fda76627cefd7dd7995812051bfa2daa150ab
pchchv <ipchchv@gmail.com>
2023-12-25 18:37:42 +0300

    Implement a NewTrace function

--- stack/stack.go
+++ stack/stack.go
@@ -1,6 +1,18 @@
 package stack
 
+import "runtime"
+
+var MaxStackDepth = 50 // maximum number of stackframes on any error
+
 type Trace struct {
 	stack  []uintptr
 	frames []Frame
+}
+
+func NewTrace(skip int) *Trace {
+	stack := make([]uintptr, MaxStackDepth)
+	length := runtime.Callers(2+skip, stack)
+	return &Trace{
+		stack: stack[:length],
+	}
 }
