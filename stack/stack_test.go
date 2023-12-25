93eaca9d209fe15befc55ed4afa3c21449a51310
pchchv <ipchchv@gmail.com>
2023-12-25 18:40:39 +0300

    Implement a level0, level1 and level2 methods

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -1,3 +1,15 @@
 package stack
 
 type someT struct{}
+
+func (s someT) level2() *Trace {
+	return NewTrace(0)
+}
+
+func (s someT) level1() *Trace {
+	return s.level2()
+}
+
+func (s someT) level0() *Trace {
+	return s.level1()
+}
