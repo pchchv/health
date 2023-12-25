6c052ae5a213c7b3ed0fda507fb2a8e8931837fe
pchchv <ipchchv@gmail.com>
2023-12-25 18:41:02 +0300

    Implement a level0, level1 and level2 funcs

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -13,3 +13,15 @@ func (s someT) level1() *Trace {
 func (s someT) level0() *Trace {
 	return s.level1()
 }
+
+func level2() *Trace {
+	return NewTrace(0)
+}
+
+func level1() *Trace {
+	return level2()
+}
+
+func level0() *Trace {
+	return level1()
+}
