2790e0799ff6afb6f0bc11c4b9c200632a8dd5dc
pchchv <ipchchv@gmail.com>
2023-12-25 18:32:39 +0300

    Implement a packageAndName function

--- stack/frame.go
+++ stack/frame.go
@@ -1,5 +1,10 @@
 package stack
 
+import (
+	"runtime"
+	"strings"
+)
+
 // Frame contains all necessary information about to generate a line in a callstack.
 type Frame struct {
 	File            string
@@ -8,4 +13,19 @@ type Frame struct {
 	Package         string
 	IsSystemPackage bool
 	ProgramCounter  uintptr
+}
+
+func packageAndName(fn *runtime.Func) (pkg string, name string) {
+	name = fn.Name()
+	// first remove the path prefix if there is one
+	if lastslash := strings.LastIndex(name, "/"); lastslash >= 0 {
+		pkg += name[:lastslash] + "/"
+		name = name[lastslash+1:]
+	}
+
+	if period := strings.Index(name, "."); period >= 0 {
+		pkg += name[:period]
+		name = name[period+1:]
+	}
+	return
 }
