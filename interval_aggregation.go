9a8d5ac9f6ebc1e7e5e241b016c1eaf2cca74608
pchchv <ipchchv@gmail.com>
2023-12-26 14:34:37 +0300

    Implement an incrementAndAddError and addError methods

--- interval_aggregation.go
+++ interval_aggregation.go
@@ -1,5 +1,7 @@
 package health
 
+import "reflect"
+
 type TimerAggregation struct {
 	Count           int64   `json:"count"`
 	NanosSum        int64   `json:"nanos_sum"`
@@ -27,4 +29,20 @@ type ErrorCounter struct {
 	// Let's keep a ring buffer of some errors. I feel like this isn't the best data structure / plan of attack here but works for now.
 	errorSamples     [5]error
 	errorSampleIndex int
+}
+
+func (ec *ErrorCounter) incrementAndAddError(inputErr error) {
+	ec.Count++
+	ec.addError(inputErr)
+}
+
+func (ec *ErrorCounter) addError(inputErr error) {
+	lastErr := ec.errorSamples[ec.errorSampleIndex]
+	if lastErr == nil {
+		ec.errorSamples[ec.errorSampleIndex] = inputErr
+	} else if !reflect.DeepEqual(lastErr, inputErr) {
+		n := len(ec.errorSamples)
+		ec.errorSampleIndex = (ec.errorSampleIndex + 1) % n
+		ec.errorSamples[ec.errorSampleIndex] = inputErr
+	}
 }
