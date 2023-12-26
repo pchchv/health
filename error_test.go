04beedfeb8ee4192cd04b0c75d81a27877027131
pchchv <ipchchv@gmail.com>
2023-12-26 14:26:46 +0300

    Implement an muted errors tests

--- error_test.go
+++ error_test.go
@@ -45,3 +45,39 @@ func TestUnmutedErrors(t *testing.T) {
 	assert.True(t, sink.LastErrUnmuted)
 	assert.True(t, sink.LastErrEmitted)
 }
+
+func TestMutedErrors(t *testing.T) {
+	stream := NewStream()
+	sink := &testSink{}
+	stream.AddSink(sink)
+	job := stream.NewJob("myjob")
+
+	origErr := fmt.Errorf("wat")
+	mutedOrig := Mute(origErr)
+	retErr := job.EventErr("abcd", mutedOrig)
+
+	// retErr is an UnmutedError with Emitted=true
+	if retErr, ok := retErr.(*MutedError); ok {
+		assert.Equal(t, retErr.Err, origErr)
+	} else {
+		t.Errorf("expected retErr to be an *MutedError")
+	}
+
+	// LastErr has Emitted=false, WasUnmuted=true
+	assert.NotNil(t, sink.LastErr)
+	assert.True(t, sink.LastErrMuted)
+
+	// Log it again!
+	retErr2 := job.EventErr("abcdefg", retErr)
+
+	// retErr is an UnmutedError with Emitted=true
+	if retErr2, ok := retErr2.(*MutedError); ok {
+		assert.Equal(t, retErr2.Err, origErr) // don't endlessly wrap MutedErrors inside MutedErrors
+	} else {
+		t.Errorf("expected retErr to be an *MutedError")
+	}
+
+	// LastErr has Emitted=false, WasUnmuted=true
+	assert.NotNil(t, sink.LastErr)
+	assert.True(t, sink.LastErrMuted)
+}
