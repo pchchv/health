d6915fe639b1567692593bc4bfe7ab6efcee45e9
pchchv <ipchchv@gmail.com>
2023-12-26 14:31:58 +0300

    Implement an ingest method

--- interval_aggregation.go
+++ interval_aggregation.go
@@ -7,3 +7,17 @@ type TimerAggregation struct {
 	NanosMin        int64   `json:"nanos_min"`
 	NanosMax        int64   `json:"nanos_max"`
 }
+
+func (a *TimerAggregation) ingest(nanos int64) {
+	a.Count++
+	a.NanosSum += nanos
+	floatNano := float64(nanos)
+	a.NanosSumSquares += (floatNano * floatNano)
+	if a.Count == 1 || nanos < a.NanosMin {
+		a.NanosMin = nanos
+	}
+
+	if a.Count == 1 || nanos > a.NanosMax {
+		a.NanosMax = nanos
+	}
+}
