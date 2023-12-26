6e37100d41249a723577f768e2af8678af0278e5
pchchv <ipchchv@gmail.com>
2023-12-26 14:20:25 +0300

    Implement an EmitEvent and EmitEventErr methods

--- health_test.go
+++ health_test.go
@@ -14,3 +14,28 @@ type testSink struct {
 	LastKvs        map[string]string
 	LastStatus     CompletionStatus
 }
+
+func (s *testSink) EmitEvent(job string, event string, kvs map[string]string) {
+	s.LastEmitKind = "Event"
+	s.LastJob = job
+	s.LastEvent = event
+	s.LastKvs = kvs
+}
+
+func (s *testSink) EmitEventErr(job string, event string, inputErr error, kvs map[string]string) {
+	s.LastEmitKind = "EventErr"
+	s.LastJob = job
+	s.LastEvent = event
+	s.LastKvs = kvs
+	s.LastErr = inputErr
+
+	switch inputErr := inputErr.(type) {
+	case *UnmutedError:
+		s.LastErrUnmuted = true
+		s.LastErrEmitted = inputErr.Emitted
+	case *MutedError:
+		s.LastErrMuted = true
+	default: // eg, case error:
+		s.LastErrRaw = true
+	}
+}
