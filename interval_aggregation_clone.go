4506432ece2003d2f4c66cca9f9263bc7724f1c9
pchchv <ipchchv@gmail.com>
2023-12-27 08:45:34 +0300

    Implement a Clone method of the aggregationMaps

--- interval_aggregation_clone.go
+++ interval_aggregation_clone.go
@@ -9,3 +9,24 @@ func (ta *TimerAggregation) Clone() *TimerAggregation {
 	var dup = *ta
 	return &dup
 }
+
+func (am *aggregationMaps) Clone() *aggregationMaps {
+	dup := &aggregationMaps{}
+	dup.initAggregationMaps()
+	for k, v := range am.Events {
+		dup.Events[k] = v
+	}
+
+	for k, v := range am.Gauges {
+		dup.Gauges[k] = v
+	}
+
+	for k, v := range am.Timers {
+		dup.Timers[k] = v.Clone()
+	}
+
+	for k, v := range am.EventErrs {
+		dup.EventErrs[k] = v.Clone()
+	}
+	return dup
+}
