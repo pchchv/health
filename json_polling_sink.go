3d1da61f8d98804abce3dd4f49da94706926695d
pchchv <ipchchv@gmail.com>
2023-12-27 09:05:07 +0300

    Add a JsonPollingSink type

--- json_polling_sink.go
+++ json_polling_sink.go
@@ -1,5 +1,7 @@
 package health
 
+import "time"
+
 const (
 	cmdKindEvent cmdKind = iota
 	cmdKindEventErr
@@ -18,4 +20,12 @@ type emitCmd struct {
 	Nanos  int64
 	Value  float64
 	Status CompletionStatus
+}
+
+type JsonPollingSink struct {
+	intervalDuration  time.Duration
+	cmdChan           chan *emitCmd
+	doneChan          chan int
+	doneDoneChan      chan int
+	intervalsChanChan chan chan []*IntervalAggregation
 }
