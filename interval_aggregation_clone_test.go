367e0a3e3ec6082a67ea4ce530122ef0578f6b02
pchchv <ipchchv@gmail.com>
2023-12-27 09:07:18 +0300

    Implement a aggregatorWithData function

--- interval_aggregation_clone_test.go
+++ interval_aggregation_clone_test.go
@@ -2,7 +2,9 @@ package health
 
 import (
 	"errors"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -52,4 +54,81 @@ func assertAggregationData(t *testing.T, intAgg *IntervalAggregation) {
 	assert.EqualValues(t, 0, intAgg.Events["bar"])
 	assert.Nil(t, intAgg.Timers["bar"])
 	assert.Nil(t, intAgg.EventErrs["bar"])
+}
+
+func aggregatorWithData() *aggregator {
+	a := newAggregator(time.Minute, time.Minute*5)
+
+	// Need 300 jobs
+	// Each job will have 5 events, but we want 1200 events total
+	// Each job will have 5 timers, but we want 1200 timers total
+	// Each job will have 5 gauges, but we want 1200 gauges total
+	// Each job will have 5 errs, but we want 1200 errs total
+	// Given this 300/1200 dichotomy,
+	//  - the first job will have 4 events, the next job 4 events, etc.
+
+	jobs := []string{}
+	for i := 0; i < 300; i++ {
+		jobs = append(jobs, fmt.Sprintf("job%d", i))
+	}
+
+	events := []string{}
+	for i := 0; i < 1200; i++ {
+		events = append(events, fmt.Sprintf("event%d", i))
+	}
+
+	timings := []string{}
+	for i := 0; i < 1200; i++ {
+		timings = append(timings, fmt.Sprintf("timing%d", i))
+	}
+
+	gauges := []string{}
+	for i := 0; i < 1200; i++ {
+		gauges = append(gauges, fmt.Sprintf("gauge%d", i))
+	}
+
+	eventErrs := []eventErr{}
+	for i := 0; i < 1200; i++ {
+		eventErrs = append(eventErrs, eventErr{
+			event: fmt.Sprintf("err%d", i),
+			err:   errors.New("wat"),
+		})
+	}
+
+	cur := 0
+	for _, j := range jobs {
+		for i := 0; i < 4; i++ {
+			a.EmitEvent(j, events[cur])
+			cur++
+		}
+	}
+
+	cur = 0
+	for _, j := range jobs {
+		for i := 0; i < 4; i++ {
+			a.EmitEventErr(j, eventErrs[cur].event, eventErrs[cur].err)
+			cur++
+		}
+	}
+
+	cur = 0
+	for _, j := range jobs {
+		for i := 0; i < 4; i++ {
+			a.EmitTiming(j, timings[cur], 12)
+			cur++
+		}
+	}
+
+	cur = 0
+	for _, j := range jobs {
+		for i := 0; i < 4; i++ {
+			a.EmitGauge(j, gauges[cur], 3.14)
+			cur++
+		}
+	}
+
+	for _, j := range jobs {
+		a.EmitComplete(j, Success, 12)
+	}
+	return a
 }
