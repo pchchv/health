2ff27a72e1cc3995a3704aeb24be969d8d270c44
pchchv <ipchchv@gmail.com>
2023-12-27 08:44:42 +0300

    Implement a Clone method of the TimerAggregation

--- interval_aggregation_clone.go
+++ interval_aggregation_clone.go
@@ -3,3 +3,9 @@ package health
 func (ec *ErrorCounter) Clone() *ErrorCounter {
 	var dup = *ec
 	return &dup
+}
+
+func (ta *TimerAggregation) Clone() *TimerAggregation {
+	var dup = *ta
+	return &dup
+}
