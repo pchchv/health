a52e6299c99f957042d20b644c4314d7b2c03b01
pchchv <ipchchv@gmail.com>
2023-12-27 09:04:40 +0300

    Implement an emitCmd type

--- json_polling_sink.go
+++ json_polling_sink.go
@@ -1,3 +1,21 @@
 package health
 
+const (
+	cmdKindEvent cmdKind = iota
+	cmdKindEventErr
+	cmdKindTiming
+	cmdKindGauge
+	cmdKindComplete
+)
+
 type cmdKind int
+
+type emitCmd struct {
+	Kind   cmdKind
+	Job    string
+	Event  string
+	Err    error
+	Nanos  int64
+	Value  float64
+	Status CompletionStatus
+}
