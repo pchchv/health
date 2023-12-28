aa9ffa84563af93b876a86137977f5f981700e7c
pchchv <ipchchv@gmail.com>
2023-12-28 12:57:59 +0300

    Implement a StartServer method

--- json_polling_sink_http.go
+++ json_polling_sink_http.go
@@ -1,9 +1,16 @@
 package health
 
-import "time"
+import (
+	"net/http"
+	"time"
+)
 
 type HealthAggregationsResponse struct {
 	InstanceId           string                 `json:"instance_id"`
 	IntervalDuration     time.Duration          `json:"interval_duration"`
 	IntervalAggregations []*IntervalAggregation `json:"aggregations"`
+}
+
+func (s *JsonPollingSink) StartServer(addr string) {
+	go http.ListenAndServe(addr, s)
 }
