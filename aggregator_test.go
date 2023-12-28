4a158b2ff5cb42ebc21ffa17af258fddc038e653
pchchv <ipchchv@gmail.com>
2023-12-28 12:23:05 +0300

    Implement a emit event error tests

--- aggregator_test.go
+++ aggregator_test.go
@@ -1,6 +1,7 @@
 package health
 
 import (
+	"errors"
 	"testing"
 	"time"
 
@@ -69,4 +70,45 @@ func TestEmitEvent(t *testing.T) {
 	intAgg = a.intervalAggregations[1]
 	assert.EqualValues(t, 1, intAgg.Events["bar"])
 	assert.EqualValues(t, 1, intAgg.SerialNumber)
+}
+
+func TestEmitEventErr(t *testing.T) {
+	setNowMock("2011-09-09T23:36:13Z")
+	defer resetNowMock()
+	a := newAggregator(time.Minute, time.Minute*5)
+	a.EmitEventErr("foo", "bar", errors.New("wat"))
+
+	assert.Equal(t, 1, len(a.intervalAggregations))
+
+	intAgg := a.intervalAggregations[0]
+	assert.NotNil(t, intAgg.EventErrs)
+	ce := intAgg.EventErrs["bar"]
+	assert.NotNil(t, ce)
+	assert.EqualValues(t, 1, ce.Count)
+	assert.Equal(t, []error{errors.New("wat")}, ce.getErrorSamples())
+	assert.EqualValues(t, 1, intAgg.SerialNumber)
+
+	assert.NotNil(t, intAgg.Jobs)
+	jobAgg := intAgg.Jobs["foo"]
+	assert.NotNil(t, jobAgg)
+	assert.NotNil(t, jobAgg.EventErrs)
+	ce = jobAgg.EventErrs["bar"]
+	assert.EqualValues(t, 1, ce.Count)
+	assert.Equal(t, []error{errors.New("wat")}, ce.getErrorSamples())
+
+	// One more event with the same error:
+	a.EmitEventErr("foo", "bar", errors.New("wat"))
+
+	intAgg = a.intervalAggregations[0]
+	ce = intAgg.EventErrs["bar"]
+	assert.EqualValues(t, 2, ce.Count)
+	assert.Equal(t, []error{errors.New("wat")}, ce.getErrorSamples()) // doesn't change
+
+	// One more event with diff error:
+	a.EmitEventErr("foo", "bar", errors.New("lol"))
+
+	intAgg = a.intervalAggregations[0]
+	ce = intAgg.EventErrs["bar"]
+	assert.EqualValues(t, 3, ce.Count)
+	assert.Equal(t, []error{errors.New("wat"), errors.New("lol")}, ce.getErrorSamples()) // new error added
 }
