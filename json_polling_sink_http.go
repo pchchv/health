2896cc175b50368015fd071ff42f13b584752d1c
pchchv <ipchchv@gmail.com>
2023-12-28 12:58:42 +0300

    Implement a renderNotFound and renderError functions

--- json_polling_sink_http.go
+++ json_polling_sink_http.go
@@ -1,6 +1,7 @@
 package health
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -13,4 +14,14 @@ type HealthAggregationsResponse struct {
 
 func (s *JsonPollingSink) StartServer(addr string) {
 	go http.ListenAndServe(addr, s)
+}
+
+func renderNotFound(rw http.ResponseWriter) {
+	rw.WriteHeader(404)
+	fmt.Fprintf(rw, `{"error": "not_found"}`)
+}
+
+func renderError(rw http.ResponseWriter, err error) {
+	rw.WriteHeader(500)
+	fmt.Fprintf(rw, `{"error": "%s"}`, err.Error())
 }
