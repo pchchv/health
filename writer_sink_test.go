826300f598479ebef16d1a8cdcdd0ff1cc11d2a6
pchchv <ipchchv@gmail.com>
2024-01-16 10:35:15 +0300

    Implement a writer sink emit timing basic tests

--- writer_sink_test.go
+++ writer_sink_test.go
@@ -13,6 +13,7 @@ var (
 	testErr             = errors.New("test error")
 	basicEventRegexp    = regexp.MustCompile("\\[[^\\]]+\\]: job:(.+) event:(.+)")
 	basicEventErrRegexp = regexp.MustCompile("\\[[^\\]]+\\]: job:(.+) event:(.+) err:(.+)")
+	basicTimingRegexp   = regexp.MustCompile("\\[[^\\]]+\\]: job:(.+) event:(.+) time:(.+)")
 	kvsEventRegexp      = regexp.MustCompile("\\[[^\\]]+\\]: job:(.+) event:(.+) kvs:\\[(.+)\\]")
 	kvsEventErrRegexp   = regexp.MustCompile("\\[[^\\]]+\\]: job:(.+) event:(.+) err:(.+) kvs:\\[(.+)\\]")
 )
@@ -71,4 +72,18 @@ func TestWriterSinkEmitEventErrKvs(t *testing.T) {
 	assert.Equal(t, "myevent", result[2])
 	assert.Equal(t, testErr.Error(), result[3])
 	assert.Equal(t, "another:thing wat:ok", result[4])
+}
+
+func TestWriterSinkEmitTimingBasic(t *testing.T) {
+	var b bytes.Buffer
+	sink := WriterSink{&b}
+	sink.EmitTiming("myjob", "myevent", 1204000, nil)
+
+	str := b.String()
+
+	result := basicTimingRegexp.FindStringSubmatch(str)
+	assert.Equal(t, 4, len(result))
+	assert.Equal(t, "myjob", result[1])
+	assert.Equal(t, "myevent", result[2])
+	assert.Equal(t, "1204 μs", result[3])
 }
