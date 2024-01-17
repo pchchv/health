017d3cfb7d8c9963c97e2ea0e492cbfccba1d56d
pchchv <ipchchv@gmail.com>
2024-01-17 13:00:11 +0300

    Add a StatsDSinkOptions type

--- statsd_sink.go
+++ statsd_sink.go
@@ -14,3 +14,19 @@ type prefixBuffer struct {
 	*bytes.Buffer
 	prefixLen int
 }
+
+type StatsDSinkOptions struct {
+	// Prefix is something like "metroid"
+	// Events emitted to StatsD would be metroid.myevent.wat
+	// Eg, don't include a trailing dot in the prefix.
+	// It can be "", that's fine.
+	Prefix string
+	// SanitizationFunc sanitizes jobs and events before sending them to statsd
+	SanitizationFunc StatsDSinkSanitizationFunc
+	// SkipNestedEvents will skip {events,timers,gauges} from sending the job.event version
+	// and will only send the event version.
+	SkipNestedEvents bool
+	// SkipTopLevelEvents will skip {events,timers,gauges} from sending the event version
+	// and will only send the job.event version.
+	SkipTopLevelEvents bool
+}
