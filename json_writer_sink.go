efc8f418821f835b4037c9e2efe48cecf78822b8
pchchv <ipchchv@gmail.com>
2024-01-18 11:38:58 +0300

    Implement an EmitEvent and EmitEventErr methods

--- json_writer_sink.go
+++ json_writer_sink.go
@@ -1,7 +1,39 @@
 package health
 
-import "io"
+import (
+	"encoding/json"
+	"io"
+)
 
 type JsonWriterSink struct {
 	io.Writer
+}
+
+func (j *JsonWriterSink) EmitEvent(job string, event string, kvs map[string]string) {
+	b, err := json.Marshal(struct {
+		Job       string
+		Event     string
+		Timestamp string
+		Kvs       map[string]string
+	}{job, event, timestamp(), kvs})
+
+	if err != nil {
+		return
+	}
+	j.Write(b)
+}
+
+func (j *JsonWriterSink) EmitEventErr(job string, event string, err error, kvs map[string]string) {
+	b, err := json.Marshal(struct {
+		Job       string
+		Event     string
+		Timestamp string
+		Err       string
+		Kvs       map[string]string
+	}{job, event, timestamp(), err.Error(), kvs})
+
+	if err != nil {
+		return
+	}
+	j.Write(b)
 }
