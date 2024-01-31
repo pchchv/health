d4ad0e080af8794d95324f2cb0265b4392359b4f
pchchv <ipchchv@gmail.com>
2024-01-31 12:54:41 +0300

    Implement a testAggregationsOverall testJobs funcs

--- healthd/healthd_test.go
+++ healthd/healthd_test.go
@@ -35,3 +35,44 @@ func testAggregations(t *testing.T, hd *HealthD) {
 	assert.Equal(t, len(resp.Aggregations), 1)
 	assertFooBarAggregation(t, resp.Aggregations[0])
 }
+
+func testAggregationsOverall(t *testing.T, hd *HealthD) {
+	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/healthd/aggregations/overall", nil)
+	hd.apiRouter().ServeHTTP(recorder, request)
+	assert.Equal(t, 200, recorder.Code)
+
+	var resp ApiResponseAggregationsOverall
+	err := json.Unmarshal(recorder.Body.Bytes(), &resp)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, resp.Overall)
+	assertFooBarAggregation(t, resp.Overall)
+}
+
+func testJobs(t *testing.T, hd *HealthD) {
+	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/healthd/jobs", nil)
+	hd.apiRouter().ServeHTTP(recorder, request)
+	assert.Equal(t, 200, recorder.Code)
+
+	var resp ApiResponseJobs
+	err := json.Unmarshal(recorder.Body.Bytes(), &resp)
+
+	assert.NoError(t, err)
+	assert.Equal(t, len(resp.Jobs), 1)
+	job := resp.Jobs[0]
+	assert.Equal(t, job.Name, "foo")
+	assert.EqualValues(t, job.Count, 2)
+	assert.EqualValues(t, job.CountSuccess, 1)
+	assert.EqualValues(t, job.CountValidationError, 1)
+	assert.EqualValues(t, job.CountError, 0)
+	assert.EqualValues(t, job.CountPanic, 0)
+	assert.EqualValues(t, job.CountJunk, 0)
+	assert.EqualValues(t, job.NanosSum, 14443)
+	assert.EqualValues(t, job.NanosMin, 5678)
+	assert.EqualValues(t, job.NanosMax, 8765)
+	assert.InDelta(t, job.NanosAvg, 7221.5, 0.01)
+	assert.InDelta(t, job.NanosSumSquares, 1.09064909e+08, 0.01)
+	assert.InDelta(t, job.NanosStdDev, 2182.8386, 0.01)
+}
