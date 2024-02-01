98899a5493f0ce9e82cdbc493d686088b87a3000
pchchv <ipchchv@gmail.com>
2024-02-01 13:58:02 +0300

    Implement a Report method

--- runtime_metrics/runtime_metrics.go
+++ runtime_metrics/runtime_metrics.go
@@ -1,6 +1,8 @@
 package runtime_metrics
 
 import (
+	"runtime"
+	"runtime/debug"
 	"time"
 
 	"github.com/pchchv/health"
@@ -35,6 +37,47 @@ func NewRuntimeMetrics(stream health.EventReceiver, options *Options) *RuntimeMe
 		rm.options = Options{time.Second * 5, true, true, true, true, true, true}
 	}
 	return rm
+}
+
+func (rm *RuntimeMetrics) Report() {
+	var mem runtime.MemStats
+	runtime.ReadMemStats(&mem)
+
+	if rm.options.Memory {
+		// bytes allocated and not yet freed
+		rm.reportGauge("alloc", float64(mem.Alloc))
+		// total number of allocated objects
+		rm.reportGauge("heap_objects", float64(mem.HeapObjects))
+	}
+
+	if rm.options.GC {
+		rm.reportGauge("pause_total_ns", float64(mem.PauseTotalNs))
+		rm.reportGauge("num_gc", float64(mem.NumGC))
+		rm.reportGauge("next_gc", float64(mem.NextGC))
+		rm.reportGauge("gc_cpu_fraction", mem.GCCPUFraction)
+	}
+
+	if rm.options.GCQuantile {
+		var gc debug.GCStats
+		gc.PauseQuantiles = make([]time.Duration, 3)
+		debug.ReadGCStats(&gc)
+		rm.reportGauge("gc_pause_quantile_50", float64(gc.PauseQuantiles[1]/1000)/1000.0)
+		rm.reportGauge("gc_pause_quantile_max", float64(gc.PauseQuantiles[2]/1000)/1000.0)
+	}
+
+	if rm.options.Goroutines {
+		rm.reportGauge("num_goroutines", float64(runtime.NumGoroutine()))
+	}
+
+	if rm.options.Cgo {
+		rm.reportGauge("num_cgo_call", float64(runtime.NumCgoCall()))
+	}
+
+	if rm.options.FDs {
+		if num, err := getFDUsage(); err == nil {
+			rm.reportGauge("num_fds_used", float64(num))
+		}
+	}
 }
 
 func (rm *RuntimeMetrics) reportGauge(key string, val float64) {
