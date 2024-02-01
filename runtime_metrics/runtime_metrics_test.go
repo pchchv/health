ddb0140796b8903ddcd8fa2aee06c3476065a744
pchchv <ipchchv@gmail.com>
2024-02-01 14:23:02 +0300

    Implement an Event, EventKv and EventErr methods

--- runtime_metrics/runtime_metrics_test.go
+++ runtime_metrics/runtime_metrics_test.go
@@ -3,3 +3,11 @@ package runtime_metrics
 type testReceiver struct {
 	gauges map[string]float64
 }
+
+func (t *testReceiver) Event(eventName string) {}
+
+func (t *testReceiver) EventKv(eventName string, kvs map[string]string) {}
+
+func (t *testReceiver) EventErr(eventName string, err error) error {
+	return nil
+}
