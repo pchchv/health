bc255bca6915f0ec4348888d794f6188fa9ef208
pchchv <ipchchv@gmail.com>
2024-02-01 14:26:20 +0300

    Implement a runtime metrics tests

--- runtime_metrics/runtime_metrics_test.go
+++ runtime_metrics/runtime_metrics_test.go
@@ -1,5 +1,7 @@
 package runtime_metrics
 
+import "testing"
+
 type testReceiver struct {
 	gauges map[string]float64
 }
@@ -24,4 +26,23 @@ func (t *testReceiver) GaugeKv(eventName string, value float64, kvs map[string]s
 
 func (t *testReceiver) Gauge(eventName string, value float64) {
 	t.gauges[eventName] = value
+}
+
+func TestRuntimeMetrics(t *testing.T) {
+	tr := &testReceiver{
+		gauges: make(map[string]float64),
+	}
+	m := NewRuntimeMetrics(tr, nil)
+	m.Start()
+	defer m.Stop()
+	m.Report()
+
+	expectedKeys := []string{
+		"heap_objects", "alloc", "num_gc", "next_gc", "gc_cpu_fraction", "pause_total_ns",
+		"gc_pause_quantile_50", "gc_pause_quantile_max", "num_cgo_call", "num_goroutines", "num_fds_used"}
+	for _, k := range expectedKeys {
+		if _, ok := tr.gauges[k]; !ok {
+			t.Errorf("expected to have key %s but didn't. map=%v", k, tr.gauges)
+		}
+	}
 }
