63c389da143cae9d34176f302df6ab16a267c5e8
pchchv <ipchchv@gmail.com>
2024-02-01 14:24:18 +0300

    Implement an EventErrKv, Timing and TimingKv methods

--- runtime_metrics/runtime_metrics_test.go
+++ runtime_metrics/runtime_metrics_test.go
@@ -11,3 +11,11 @@ func (t *testReceiver) EventKv(eventName string, kvs map[string]string) {}
 func (t *testReceiver) EventErr(eventName string, err error) error {
 	return nil
 }
+
+func (t *testReceiver) EventErrKv(eventName string, err error, kvs map[string]string) error {
+	return nil
+}
+
+func (t *testReceiver) Timing(eventName string, nanoseconds int64) {}
+
+func (t *testReceiver) TimingKv(eventName string, nanoseconds int64, kvs map[string]string) {}
