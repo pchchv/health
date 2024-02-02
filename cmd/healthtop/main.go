bcf6bf726b278dc3f444095ba9efb20593587aef
pchchv <ipchchv@gmail.com>
2024-02-02 14:20:07 +0300

    Implement a FmtNow and FmtStatus methods

--- cmd/healthtop/main.go
+++ cmd/healthtop/main.go
@@ -1,11 +1,28 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type healthdStatus struct {
 	lastSuccessAt time.Time
 	lastErrorAt   time.Time
 	lastError     error
+}
+
+func (s *healthdStatus) FmtNow() string {
+	return time.Now().Format(time.RFC1123)
+}
+
+func (s *healthdStatus) FmtStatus() string {
+	if s.lastErrorAt.IsZero() && s.lastSuccessAt.IsZero() {
+		return "[starting...]"
+	} else if s.lastErrorAt.After(s.lastSuccessAt) {
+		return fmt.Sprint("[error: '", s.lastError.Error(), "'    LastErrorAt: ", s.lastErrorAt.Format(time.RFC1123), "]")
+	} else {
+		return "[success]"
+	}
 }
 
 func main() {}
