6cb5aed2cc148dc447753eb3bc4bba1b59254d3f
pchchv <ipchchv@gmail.com>
2024-02-05 11:41:40 +0300

    Implement a notify tests

--- sinks/bugsnag/api_test.go
+++ sinks/bugsnag/api_test.go
@@ -5,6 +5,12 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/pchchv/health/stack"
+	"github.com/stretchr/testify/assert"
 )
 
 type notifyHandler struct {
@@ -28,4 +34,47 @@ func (h notifyHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	h.PayloadChan <- &resp
 
 	fmt.Fprintf(rw, "OK")
+}
+
+func TestNotify(t *testing.T) {
+	config := &Config{
+		APIKey:       "abcd",
+		Endpoint:     "http://localhost:5051/",
+		ReleaseStage: "staging",
+		AppVersion:   "1.0",
+		Hostname:     "",
+	}
+
+	n := notifyHandler{
+		PayloadChan: make(chan *payload, 1),
+	}
+
+	go http.ListenAndServe(":5051", n)
+	time.Sleep(10 * time.Millisecond)
+
+	err := Notify(config, "users/get", "foo.bar", fmt.Errorf("imanerror"), stack.NewTrace(0), make(map[string]string))
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	p := <-n.PayloadChan
+
+	assert.NotNil(t, p)
+	assert.Equal(t, p.APIKey, "abcd")
+	assert.Equal(t, p.Notifier.Name, "health")
+	assert.Equal(t, len(p.Events), 1)
+
+	evt := p.Events[0]
+	assert.Equal(t, evt.Context, "users/get")
+	assert.Equal(t, evt.App.ReleaseStage, "staging")
+	assert.Equal(t, len(evt.Exceptions), 1)
+
+	ex := evt.Exceptions[0]
+	assert.Equal(t, ex.ErrorClass, "foo.bar")
+	assert.Equal(t, ex.Message, "imanerror")
+
+	frame := ex.Stacktrace[0]
+	assert.True(t, strings.HasSuffix(frame.File, "api_test.go"))
+	assert.Equal(t, frame.Method, "github.com/pchchv/health/sinks/bugsnag:TestNotify")
+
 }
