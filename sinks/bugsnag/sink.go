507fccf21bb566e6475e5a9f6821ddbd2694b2f9
pchchv <ipchchv@gmail.com>
2024-02-05 11:44:08 +0300

    Implement a errorProcessingLoop function

--- sinks/bugsnag/sink.go
+++ sinks/bugsnag/sink.go
@@ -1,6 +1,11 @@
 package bugsnag
 
-import "github.com/pchchv/health"
+import (
+	"fmt"
+	"os"
+
+	"github.com/pchchv/health"
+)
 
 type cmdEventErr struct {
 	Job   string
@@ -14,4 +19,21 @@ type Sink struct {
 	*Config
 	cmdChan  chan *cmdEventErr
 	doneChan chan int
+}
+
+func errorProcessingLoop(sink *Sink) {
+	cmdChan := sink.cmdChan
+	doneChan := sink.doneChan
+
+PROCESSING_LOOP:
+	for {
+		select {
+		case <-doneChan:
+			break PROCESSING_LOOP
+		case cmd := <-cmdChan:
+			if err := Notify(sink.Config, cmd.Job, cmd.Event, cmd.Err, cmd.Err.Stack, cmd.Kvs); err != nil {
+				fmt.Fprintf(os.Stderr, "bugsnag.Notify: could not notify bugsnag. err=%v\n", err)
+			}
+		}
+	}
 }
