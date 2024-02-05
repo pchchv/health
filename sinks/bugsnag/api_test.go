d346da3bfdd14645bba2910e2f5dbcd98fda0e01
pchchv <ipchchv@gmail.com>
2024-02-05 11:40:36 +0300

    Implement a ServeHTTP method

--- sinks/bugsnag/api_test.go
+++ sinks/bugsnag/api_test.go
@@ -1,5 +1,31 @@
 package bugsnag
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
 type notifyHandler struct {
 	PayloadChan chan *payload
+}
+
+func (h notifyHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(rw, "got error in ready body: %v", err)
+		return
+	}
+
+	var resp payload
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		fmt.Fprintf(rw, "got error in unmarshal: %v", err)
+		return
+	}
+
+	h.PayloadChan <- &resp
+
+	fmt.Fprintf(rw, "OK")
 }
